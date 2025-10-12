2f4bc4d662ef24d0abd022a5d650b0cb26737bc4
Praveen Diwakar <praveendiwakar.in@gmail.com>
2025-10-12 16:29:36 +0530

    Update file_size.py
    Updated New Features By 𝕏Ð

--- clouddroid/utils/file_size.py
+++ clouddroid/utils/file_size.py
@@ -1,5 +1,6 @@
-# (c) @biisal
-# (c) adarsh-goel
+# <!-- # (c) @clouddroid
+# (c) Praveen(𝕏Ð)Diwakar-->
 def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
     """ Returns a human readable string representation of bytes """
     return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
+
